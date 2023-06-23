3032da89afe8d27cb1c485d4a9f7a73dce5ea13b
Volha Vybarnaya <olyaviborn@gmail.com>
2023-06-23 10:38:05 +0300

    добавиа задачу 0

---
+++ 0/Program.cs
@@ -0,0 +1,18 @@
+﻿// **Задание 1:**Напишите программу,
+//  которая на вход принимает два числа и выдаёт,
+//   какое число большее,.
+
+Console.Clear();
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+if ( a > b)
+{
+    Console.WriteLine($"Максимальное число {a}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число {b}");
+}
