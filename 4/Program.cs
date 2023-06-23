c91a18d5655e1ab4137f229b19bee7207a4b12d1
Volha Vybarnaya <olyaviborn@gmail.com>
2023-06-23 10:45:49 +0300

    добавиа задачу 4

---
+++ 4/Program.cs
@@ -0,0 +1,20 @@
+﻿// * *Задание 4:**Напишите программу, 
+// которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+// int i = 1;
+for ( int i = 1; i<=N; i++ )
+{
+    if ( i%2==0)
+    {
+        Console.WriteLine($"{i}  ");
+    }
+}
+
+
+
+
+
+
